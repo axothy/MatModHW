deb6545fdf22d61b5c2d384ba5240c6afa97b659
Alexander Chebotin <61747868+axothy@users.noreply.github.com>
2021-10-15 20:01:28 +0300

    Update LA.cpp

--- LA.cpp
+++ LA.cpp
@@ -7,12 +7,14 @@
 #include <algorithm>
 #include <cmath>
 #include <io.h>
+#include <stdint.h>
+
+#include <chrono>
 
 typedef std::pair<double, double> Point;
 typedef std::vector<Point> ListOfPoints;
 typedef std::vector<double> Distances;
 typedef std::pair<Point, double> PointAndDistance;
-typedef std::vector<PointAndDistance> ListOfPointAndDistances;
 
 struct Line {
 	double x0, y0;
@@ -35,7 +37,6 @@ private:
 
 	Distances distances;
 	PointAndDistance point_and_distance;
-	ListOfPointAndDistances list_of_points_and_distances;
 
 public:
 
@@ -46,7 +47,7 @@ public:
 	~PointsSearching() { file.close(); }
 
 	inline void getVectors();
-	 void searchLeftRight();
+	void searchLeftRight();
 };
 
 const std::string PointsSearching::filename = "in.txt";
@@ -63,6 +64,8 @@ PointsSearching::PointsSearching()
 	line.A = line.yn - line.y0;
 	line.B = line.x0 - line.xn;
 	line.C = line.xn * line.y0 - line.x0 * line.yn;
+
+	std::cout << line.A << " " << line.B << " " << line.C << " \n\n";
 
 	do {
 		file >> point.first >> point.second;
@@ -86,35 +89,30 @@ inline void PointsSearching::getVectors()
 	}
 }
 
- void PointsSearching::searchLeftRight()
+void PointsSearching::searchLeftRight()
 {
+	Point rightmost_point;
+	Point leftmost_point;
+
 	distances.reserve(x.size());
+	double max_distance = 0, min_distance = 0;
 
 	double sqrt_denominator = sqrt(line.A * line.A + line.B * line.B); //for algorithm efficiency
 
-	for (long int i = 0; i < x.size(); i++)
+	distances.push_back(abs(line.A * x.at(0) + line.B * y.at(0) + line.C) / (sqrt_denominator));
+	point_and_distance.first = points.at(0);
+	point_and_distance.second = distances.at(0);
+	max_distance = distances.at(0);
+	min_distance = distances.at(0);
+
+	for (int i = 1; i < x.size(); i++)
 	{
 		distances.push_back(abs(line.A * x.at(i) + line.B * y.at(i) + line.C) / (sqrt_denominator));
 		point_and_distance.first = points.at(i);
 		point_and_distance.second = distances.at(i);
-		list_of_points_and_distances.push_back(point_and_distance);
-	}
 
-	double max_distance = 0, min_distance = 0;
-
-	max_distance = *std::max_element(distances.begin(), distances.end());
-	min_distance = *std::min_element(distances.begin(), distances.end());
-
-	Point rightmost_point;
-	Point leftmost_point;
-
-	for (ListOfPointAndDistances::const_iterator it = list_of_points_and_distances.begin(); it != list_of_points_and_distances.end(); ++it)
-	{
-		if ((it->second) == max_distance)
-			rightmost_point = it->first;
-
-		if ((it->second) == min_distance)
-			leftmost_point = it->first;
+		if (max_distance < distances.at(i)) { max_distance = distances.at(i); rightmost_point = points.at(i); }
+		if (min_distance > distances.at(i)) { min_distance = distances.at(i); leftmost_point = points.at(i); }
 	}
 
 	std::cout << "Leftmost: " << leftmost_point.first << ' ' << leftmost_point.second << "\n";
@@ -124,7 +122,17 @@ inline void PointsSearching::getVectors()
 
 int main(void)
 {
+	auto start_time = std::chrono::steady_clock::now();
+
+	//начало кода для таймера
 	PointsSearching search;
 	search.searchLeftRight();
+	std::cout << "\n\n";
+	//конец кода для таймера
+
+	auto end_time = std::chrono::steady_clock::now();
+	auto elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+	std::cout << elapsed_ns.count() << " nanosec\n";
+
 	return 0;
 }
